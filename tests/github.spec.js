a0d876f73e3550cec984603288d1813a6952dc3f
Zeeshan <mohdzee1414@gmail.com>
2026-09-11 16:50:25 +0530

    gitchanges

---
+++ tests/github.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("Practice Test 1", async ({ page }) => {
+    console.log("Starting Practice Test 1");
+    await page.goto("https://www.saucedemo.com/");
+    console.log(await page.title());
+    expect(page).toHaveTitle("Swag Labs");
+    console.log("Ending Practice Test 1");
+})
+
+test("Practice Test 2", async ({ page }) => {
+    console.log("Starting Practice Test 2");
+    await page.goto("https://www.saucedemo.com/");
+    console.log(await page.title());
+    expect(page).toHaveTitle("Swag Labsass");
+    console.log("Ending Practice Test 2");
+})
